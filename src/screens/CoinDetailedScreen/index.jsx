a12281659028e4c46c0b81868d895d571603eb8d
Nobel322 <emanueljuricev5@gmail.com>
2022-02-23 16:07:41 +0100

    .

---
+++ src/screens/CoinDetailedScreen/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+const CoinDetailedScreen = () => {
+  return (
+    <View>
+      <Ionicons name="chevron-back-sharp" size={24} color="white" />
+    </View>
+  );
+};
+
+export default CoinDetailedScreen;
