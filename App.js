6b69a08562d55d10a54ea5ae455fee8f1861f792
Nobel322 <emanueljuricev5@gmail.com>
2022-02-23 01:11:41 +0100

    added main page

--- App.js
+++ App.js
@@ -1,30 +1,16 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Image } from "react-native";
+import React from "react";
+import { StyleSheet, View, FlatList } from "react-native";
+import CoinItem from "./src/components/CoinItem";
+import cryptocurrencies from "./assets/data/cryptocurrencies.json";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.coinContainer}>
-        <Image
-          source={{
-            uri: "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579",
-          }}
-          style={{
-            height: 30,
-            width: 30,
-            marginRight: 10,
-            alignSelf: "center",
-          }}
-        />
-        <View>
-          <Text style={styles.title}>Bitcoin</Text>
-          <View style={{ flexDirection: "row" }}>
-            <Text style={styles.text}>1</Text>
-            <Text style={styles.text}>BTC</Text>
-            <Text style={styles.text}>0,63%</Text>
-          </View>
-        </View>
-      </View>
+      <FlatList
+        data={cryptocurrencies}
+        renderItem={({ item }) => <CoinItem marketCoin={item} />}
+      />
       <StatusBar style="light" />
     </View>
   );
@@ -35,20 +21,5 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#121212",
     paddingTop: 50,
-  },
-  title: {
-    color: "white",
-    fontSize: 16,
-    fontWeight: "bold",
-  },
-  text: {
-    color: "white",
-    marginRight: 5,
-  },
-  coinContainer: {
-    flexDirection: "row",
-    borderBottomWidth: StyleSheet.hairlineWidth,
-    borderBottomColor: "grey",
-    padding: 15,
   },
 });
